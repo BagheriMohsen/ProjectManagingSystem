9a3afc58177ecc4480d035dc903db5b195967423
mohsen bagheri <bagheriimohsen@gmail.com>
2020-02-23 16:02:30 +0330

    moddify user migration,model

--- app/User.php
+++ app/User.php
@@ -17,11 +17,14 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
         'username',
-        'phone_number'
+        'phone_number',
+        'token',
+        'unit'
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('last_name')->nullable();
             $table->string('username');
             $table->string('phone_number')->nullable();
+            $table->string('token')->nullable();
             $table->string('unit')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
