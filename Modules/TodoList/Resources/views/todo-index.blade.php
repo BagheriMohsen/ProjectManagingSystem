65e41bf7409fdfa375321dd5e90a1d5319b51b72
mohsen bagheri <bagheriimohsen@gmail.com>
2020-03-01 20:23:24 +0330

    install vue and laravel/ui

--- Modules/TodoList/Resources/views/todo-index.blade.php
+++ Modules/TodoList/Resources/views/todo-index.blade.php
@@ -7,6 +7,19 @@
 
 @section("scripts")
     <script src="{{ asset("panel/javascript/todo.js") }}"></script>
+    <script src="{{ asset("/js/app.js") }}"></script>
+
+    <script>
+        new Vue({
+            el:"#app",
+            data(){
+                return {
+                    message:"hello world!",
+                }
+            }
+        });
+    </script>
+
 @endsection
 <!-- 
     path 
@@ -16,7 +29,6 @@
         $path = [
             'name'          =>  'Users',
             'is_modal'      =>  True,
-
         ]
     @endphp
     @include('Master.path')
@@ -32,51 +44,11 @@
     @include('Messages.errors')
     @include('Messages.message')
 
-
+    <div id="app">
+        <todo-list></todo-list>
+    </div
        
-    <div class="row row-sm">
-        <div class="col-sm-12 col-xl-12">
-            <div class="timesheet-wr">
-                <div class="row row-sm bg-gray mg-b-10">
-                    <div class="col-md-3">
-                    <form id="todo-create" action="{{ route("todolist.store") }}" >
-                        @csrf
-                            <div class="form-row">
-                                <div class="input-group">
-                                    <input name="desc" type="text" class="form-control" placeholder="+ New">
-                                    <div class="input-group-append">
-                                      <button class="btn btn-warning" type="submit"><i class="fa fa-chevron-right pd-r-3 pd-l-3"></i></button>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="row row-sm bg-white">
-                        <div class="col-md-7">
-                        <div class="card card-body bd-0 pd-25 tx-12 ">
-                            <table class="table table-hover table-bordered">
-                                @foreach($todo_lists as $todo_list)
-                                    <tr>
-                                        <td>
-                                            <label class="ckbox mp0">
-                                                <input type="checkbox">
-                                                <span>
-                                                    {{ $todo_list->desc }}
-                                                </span>
-                                            </label>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </table>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+   
 
 
 
---
+++ resources/js/components/TodoList.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+
+         <div class="row row-sm">
+        <div class="col-sm-12 col-xl-12">
+            <div class="timesheet-wr">
+                <div class="row row-sm bg-gray mg-b-10">
+                    <div class="col-md-3">
+                    <form id="todo-create" action="#" >
+                        
+                            <div class="form-row">
+                                <div class="input-group">
+                                    <input name="desc" type="text" class="form-control" placeholder="+ New">
+                                    <div class="input-group-append">
+                                      <button class="btn btn-warning" type="submit"><i class="fa fa-chevron-right pd-r-3 pd-l-3"></i></button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="row row-sm bg-white">
+                        <div class="col-md-7">
+                        <div class="card card-body bd-0 pd-25 tx-12 ">
+                            <table class="table table-hover table-bordered">
+                            
+                                    <tr v-for="todo_list in todo_lists">
+                                        <td>
+                                            <label class="ckbox mp0">
+                                                <input type="checkbox">
+                                                <span>
+                                                    {{ todo_list.desc }}
+                                                </span>
+                                            </label>
+                                        </td>
+                                    </tr> 
+                               
+                            </table>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
